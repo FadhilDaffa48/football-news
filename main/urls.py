9792add3e1f3e91576995bd36b42f0bc5b0495b7
Fadhil Daffa <ifadhil63@gmail.com>
2025-09-24 15:33:02 +0700

    tutorial 4

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import logout_user, login_user, show_main, create_news, show_news, show_xml, show_json, show_xml_by_id, show_json_by_id, register
+from main.views import delete_news, edit_news, logout_user, login_user, show_main, create_news, show_news, show_xml, show_json, show_xml_by_id, show_json_by_id, register
 app_name = 'main'
 
 urlpatterns = [
@@ -12,6 +12,8 @@ urlpatterns = [
     path('json/<str:news_id>/', show_json_by_id, name='show_json_by_id'),
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
+    path('news/<uuid:id>/edit', edit_news, name='edit_news'),
+    path('news/<uuid:id>/delete', delete_news, name='delete_news'),
     path('logout/', logout_user, name='logout'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -10,6 +10,24 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 import datetime
 
+def delete_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    news.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
+
+def edit_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    form = NewsForm(request.POST or None, instance=news)
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main:show_main')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "edit_news.html", context)
+
 def logout_user(request):
     logout(request)
     return redirect('main:login')
